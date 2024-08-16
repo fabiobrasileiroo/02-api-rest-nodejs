9fd2056420588a07434189e5bc9915fd6e2724e2
Fabio Brasileiro <fabiotrocados@debian-BULLSEYE-live-builder-AMD64>
2024-08-16 18:59:23 -0400

    estudo

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import { app } from "./app"
 import { env } from "./env"
+// host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost',
 app
   .listen({
     port: env.PORT,
+    host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
     console.log('http server Running!')
